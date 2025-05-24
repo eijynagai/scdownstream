7d73f9d9763676910832932570505314a05a1d56
nictru <nictru32@gmail.com>
2025-05-24 16:50:18 +0200

    Set scANVI random seed

--- modules/local/scvitools/scanvi/templates/scanvi.py
+++ modules/local/scvitools/scanvi/templates/scanvi.py
@@ -12,6 +12,7 @@ torch.set_float32_matmul_precision('medium')
 from threadpoolctl import threadpool_limits
 threadpool_limits(int("${task.cpus}"))
 scvi.settings.num_threads = int("${task.cpus}")
+scvi.settings.seed = 0
 
 def format_yaml_like(data: dict, indent: int = 0) -> str:
     """Formats a dictionary to a YAML-like string.
